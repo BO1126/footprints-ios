095ba4534c2262b322bf9af72f5c504e52c95e63
jungwoo Lee <b5nnnal88@gmail.com>
2022-03-08 12:27:18 +0900

    Success print footprints and change alpha

--- footprints/footprints/ViewController.swift
+++ footprints/footprints/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tapGesture : UITapGestureRecognizer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tapViewGesture(){
+        let newImageView = UIImageView()
+        newImageView.frame = CGRect(x: (tapGesture.location(in: self.view).x-40), y: (tapGesture.location(in: self.view).y-40), width: 80, height: 80)
+        newImageView.image = UIImage(named: "footprint")
+        self.view.addSubview(newImageView)
+        let time = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            UIView.animate(withDuration: 0.5, animations: {
+                newImageView.alpha = 0
+            })
+        }
     }
 
 
