a122667972105a5802fb466b33e563e95c6c092b
jungwoo Lee <b5nnnal88@gmail.com>
2022-03-08 12:00:12 +0900

    Start project

---
+++ footprints/footprints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  footprints
+//
+//  Created by 이정우 on 2022/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
